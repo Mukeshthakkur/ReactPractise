fd0dd3b6c732d562a67922cfb948d4910cc7fb9d
Mukeshthakkur <mukoographics.mg@gmail.com>
2021-04-15 18:18:40 +0500

    React project for practise

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Movies from './components/Movies'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+class App extends React.Component{
+  render(){
+    return(
+      <div>
+        <Movies />
+        
+      </div>
+    )
+  }
+}
+
+
+
+export default App;
---
+++ src/components/like.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+
+class Like extends React.Component{
+
+
+    render(){
+        let classes =" fas  fa-heart";    
+        if (!this.props.liked) classes ="far fa-heart";
+
+
+        return( 
+            <i onClick={this.props.onClick} style={{ cursor: 'pointer'}} className={classes}></i>
+        )
+    }
+}
+
+export default Like;
